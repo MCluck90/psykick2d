071b94b257287200f597a1da22095cbb6bd5720b
MCluck <mcluck90@gmail.com>
2013-09-26 09:34:53 -0600

    Merged in basic sprite rendering system

--- js/engine/components/physics/position.js
+++ js/engine/components/physics/position.js
@@ -3,25 +3,24 @@
  * @constructor
  * @inherit Psykick.Component
  * @param   {Object}    coords
- * @param   {Number}    [coords.x]  X coordinate
- * @param   {number}    [coords.y]  Y coordinate
- * @param   {number}    [coords.a]  Angle
+ * @param   {Number}    [coords.x]          X coordinate
+ * @param   {number}    [coords.y]          Y coordinate
+ * @param   {number}    [coords.rotation]   Rotation
  */
 
 Psykick.Components.Position = function(coords){
     this.Name = "Position";
 
     var defaults = {
-        //Maybe change these to center?
         x: 0,
         y: 0,
-        a: 0
+        rotation: 0
     };
 
     coords = Psykick.Helper.defaults(coords, defaults);
     this.x = coords.x;
     this.y = coords.y;
-    this.a = coords.a;
+    this.rotation = coords.rotation;
 };
 
 Psykick.Helper.extend(Psykick.Components.Position, Psykick.Component);
