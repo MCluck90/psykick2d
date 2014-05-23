3f5feb276e670436f06ee45ec06d14e1de7b41db
MCluck <mcluck90@gmail.com>
2014-05-23 16:41:03 -0600

    Removed deprecated Helpers namespace

--- src/helpers/collision-grid.js
+++
@@ -1,6 +0,0 @@
-/**
- * Used to keep backwards compatibility until v0.5.0
- * @type {CollisionGrid|exports}
- * @deprecated Use DataStructures.CollisionGrid
- */
-module.exports = require('../data-structures/collision-grid.js');
