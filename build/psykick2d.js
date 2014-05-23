8572520b818215668a8d9593b88557771bb2f258
MCluck <mcluck90@gmail.com>
2014-05-23 16:40:22 -0600

    QuadTree now uses Rectangles instead of RectPhysicsBodies

--- build/psykick2d.js
+++ build/psykick2d.js
@@ -896,8 +896,8 @@ module.exports = CollisionGrid;
 
 /**
  * Determines if two objects are colliding
- * @param {RectPhysicsBody} a
- * @param {RectPhysicsBody} b
+ * @param {Rectangle} a
+ * @param {Rectangle} b
  * @returns {boolean}
  */
 function isColliding(a, b) {
@@ -942,20 +942,20 @@ var QuadTree = function(options) {
 /**
  * Adds an entity to the tree
  * @param {Entity} entity
- * @param {RectPhysicsBody} [body]
+ * @param {RectPhysicsBody} [rect]
  */
-QuadTree.prototype.addEntity = function(entity, body) {
+QuadTree.prototype.addEntity = function(entity, rect) {
     if (this.entities.indexOf(entity) !== -1) {
         return;
     }
     if (this.w <= this.cellSize || this.h <= this.cellSize) {
         this.entities.push(entity);
     } else {
-        body = body || entity.getComponent('RectPhysicsBody');
-        var top    = body.y,
-            bottom = body.y + body.h,
-            left   = body.x,
-            right  = body.x + body.w,
+        rect = rect || entity.getComponent('RectPhysicsBody');
+        var top    = rect.y,
+            bottom = rect.y + rect.h,
+            left   = rect.x,
+            right  = rect.x + rect.w,
             inUpper = (top <= this.y + this.h / 2),
             inLower = (bottom >= this.y + this.h / 2),
             inLeft = (left <= this.x + this.w / 2),
@@ -972,7 +972,7 @@ QuadTree.prototype.addEntity = function(entity, body) {
                 nodeOptions.y = this.y;
                 this.children[0] = new QuadTree(nodeOptions);
             }
-            this.children[0].addEntity(entity, body);
+            this.children[0].addEntity(entity, rect);
         }
         if (inUpper && inRight) {
             if (!this.children[1]) {
@@ -980,7 +980,7 @@ QuadTree.prototype.addEntity = function(entity, body) {
                 nodeOptions.y = this.y;
                 this.children[1] = new QuadTree(nodeOptions);
             }
-            this.children[1].addEntity(entity, body);
+            this.children[1].addEntity(entity, rect);
         }
         if (inLower && inRight) {
             if (!this.children[2]) {
@@ -988,7 +988,7 @@ QuadTree.prototype.addEntity = function(entity, body) {
                 nodeOptions.y = this.y + this.h / 2;
                 this.children[2] = new QuadTree(nodeOptions);
             }
-            this.children[2].addEntity(entity, body);
+            this.children[2].addEntity(entity, rect);
         }
         if (inLower && inLeft) {
             if (!this.children[3]) {
@@ -996,7 +996,7 @@ QuadTree.prototype.addEntity = function(entity, body) {
                 nodeOptions.y = this.y + this.h / 2;
                 this.children[3] = new QuadTree(nodeOptions);
             }
-            this.children[3].addEntity(entity, body);
+            this.children[3].addEntity(entity, rect);
         }
     }
 };
@@ -1004,36 +1004,36 @@ QuadTree.prototype.addEntity = function(entity, body) {
 /**
  * Removes an Entity from the tree
  * @param {Entity} entity
- * @param {RectPhysicsBody} [body]
+ * @param {RectPhysicsBody} [rect]
  */
-QuadTree.prototype.removeEntity = function(entity, body) {
+QuadTree.prototype.removeEntity = function(entity, rect) {
     var entityIndex = this.entities.indexOf(entity);
     if (entityIndex !== -1) {
         this.entities.splice(entityIndex, 1);
         return;
     }
 
-    body = body || entity.getComponent('RectPhysicsBody');
-    var top    = body.y,
-        bottom = body.y + body.h,
-        left   = body.x,
-        right  = body.x + body.w,
+    rect = rect || entity.getComponent('Rectangle');
+    var top    = rect.y,
+        bottom = rect.y + rect.h,
+        left   = rect.x,
+        right  = rect.x + rect.w,
         inUpper = (top <= this.y + this.h / 2),
         inLower = (bottom >= this.y + this.h / 2),
         inLeft = (left <= this.x + this.w / 2),
         inRight = (right >= this.x + this.w / 2);
 
     if (inUpper && inLeft && this.children[0]) {
-        this.children[0].removeEntity(entity, body);
+        this.children[0].removeEntity(entity, rect);
     }
     if (inUpper && inRight && this.children[1]) {
-        this.children[1].removeEntity(entity, body);
+        this.children[1].removeEntity(entity, rect);
     }
     if (inLower && inRight && this.children[2]) {
-        this.children[2].removeEntity(entity, body);
+        this.children[2].removeEntity(entity, rect);
     }
     if (inLower && inLeft && this.children[3]) {
-        this.children[3].removeEntity(entity, body);
+        this.children[3].removeEntity(entity, rect);
     }
 };
 
@@ -1043,48 +1043,48 @@ QuadTree.prototype.removeEntity = function(entity, body) {
  * @param {{ x: number, y: number }} deltaPosition
  */
 QuadTree.prototype.moveEntity = function(entity, deltaPosition) {
-    var body = entity.getComponent('RectPhysicsBody'),
+    var rect = entity.getComponent('Rectangle'),
         hasMoved = (Math.abs(deltaPosition.x) + Math.abs(deltaPosition.y)) > 0;
 
     // TODO: Do a smart check to see if it's changed cells
     if (hasMoved) {
-        this.removeEntity(entity, body);
-        body.x += deltaPosition.x;
-        body.y += deltaPosition.y;
-        this.addEntity(entity, body);
+        this.removeEntity(entity, rect);
+        rect.x += deltaPosition.x;
+        rect.y += deltaPosition.y;
+        this.addEntity(entity, rect);
     }
 };
 
 /**
  * Returns all entities the given entity is colliding with
  * @param {Entity} entity
- * @param {RectPhysicsBody} [body]
+ * @param {Rectangle} [rect]
  * @returns {Entity[]}
  */
-QuadTree.prototype.getCollisions = function(entity, body) {
+QuadTree.prototype.getCollisions = function(entity, rect) {
     var result = [];
     if (this.entities.indexOf(entity) === -1) {
-        body = body || entity.getComponent('RectPhysicsBody');
-        var top    = body.y,
-            bottom = body.y + body.h,
-            left   = body.x,
-            right  = body.x + body.w,
+        rect = rect || entity.getComponent('Rectangle');
+        var top    = rect.y,
+            bottom = rect.y + rect.h,
+            left   = rect.x,
+            right  = rect.x + rect.w,
             inUpper = (top <= this.y + this.h / 2),
             inLower = (bottom >= this.y + this.h / 2),
             inLeft = (left <= this.x + this.w / 2),
             inRight = (right >= this.x + this.w / 2);
 
         if (inUpper && inLeft && this.children[0]) {
-            result = result.concat(this.children[0].getCollisions(entity, body));
+            result = result.concat(this.children[0].getCollisions(entity, rect));
         }
         if (inUpper && inRight && this.children[1]) {
-            result = result.concat(this.children[1].getCollisions(entity, body));
+            result = result.concat(this.children[1].getCollisions(entity, rect));
         }
         if (inLower && inRight && this.children[2]) {
-            result = result.concat(this.children[2].getCollisions(entity, body));
+            result = result.concat(this.children[2].getCollisions(entity, rect));
         }
         if (inLower && inLeft && this.children[3]) {
-            result = result.concat(this.children[3].getCollisions(entity, body));
+            result = result.concat(this.children[3].getCollisions(entity, rect));
         }
     } else {
         for (var i = 0, len = this.entities.length; i < len; i++) {
@@ -1092,7 +1092,7 @@ QuadTree.prototype.getCollisions = function(entity, body) {
             if (other === entity) {
                 continue;
             }
-            if (isColliding(body, other.getComponent('RectPhysicsBody'))) {
+            if (isColliding(rect, other.getComponent('Rectangle'))) {
                 result.push(other);
             }
         }
