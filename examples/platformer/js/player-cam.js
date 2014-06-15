dde2dc3027231d723569d64552b93716be392f40
Mike <mcluck90@gmail.com>
2014-06-15 09:03:35 -0600

    Adjusted camera to follow player like in other platformers

--- examples/platformer/js/player-cam.js
+++ examples/platformer/js/player-cam.js
@@ -9,20 +9,43 @@ var PlayerCam = function(player, width, height) {
     this._cacheY = this.playerBody.y;
     this.halfWidth = width / 2;
     this.halfHeight = height / 2;
-    this.halfPlayerWidth = this.playerBody.width / 2;
-    this.halfPlayerHeight = this.playerBody.height / 2;
+
+    // Put the camera just a little bit ahead of the player
+    this.playerXOffset = this.playerBody.width / 1.1;
+    this.playerYOffset = this.playerBody.height / 2;
 };
 
 Helper.inherit(PlayerCam, Camera);
 
+/**
+ * Moves the camera to stay focused on the player
+ * @param {Stage} stage
+ * @param {number} delta
+ */
 PlayerCam.prototype.render = function(stage, delta) {
-    var x = this.playerBody.x - this.halfWidth + this.halfPlayerWidth,
-        y = this.playerBody.y - this.halfHeight + this.halfPlayerHeight;
-    if (x !== this._cacheX || y !== this._cacheY) {
-        stage.x = -x;
-        stage.y = -y;
-        this._cacheX = x;
-        this._cacheY = y;
+    var x = this.playerBody.x - this.halfWidth + this.playerXOffset,
+        y = this.playerBody.y - this.halfHeight + this.playerYOffset;
+
+    // Make sure the screen doesn't scroll too far to the left
+    if (x !== this._cacheX) {
+        if (x > 0 && this._cacheX < 0) {
+            stage.x = 0;
+            this._cacheX = 0;
+        } else if (x > 0) {
+            stage.x = -x;
+            this._cacheX = x;
+        }
+    }
+
+    // Make sure the screen doesn't go below the bottom of the level
+    if (y !== this._cacheY) {
+        if (y > 0 && this._cacheY < 0) {
+            stage.y = 0;
+            this._cacheY = 0;
+        } else if (y < 0) {
+            stage.y = -y;
+            this._cacheY = y;
+        }
     }
 };
 
