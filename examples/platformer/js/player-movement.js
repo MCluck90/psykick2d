88f70e3f2db18fdfd97189c479982a1231059e80
Mike <mcluck90@gmail.com>
2014-07-06 14:40:08 -0600

    Fixed bug which caused players legs to spaz after dying

--- examples/platformer/js/player-movement.js
+++ examples/platformer/js/player-movement.js
@@ -289,7 +289,15 @@ PlayerMovement.prototype._states = {
         exit: function(){}
     },
     fall: {
-        enter: function() {},
+        enter: function() {
+            this.player.addComponent(this.player.getComponent('WalkAnimation'));
+            var animation = this.player.getComponent('Animation'),
+                sprite = this.player.getComponent('Sprite'),
+                body = this.player.getComponent('RectPhysicsBody');
+            animation.fps = 0;
+            sprite.setTexture(AssetManager.SpriteSheet.getFrame('player-walk3'));
+            body.friction = 0;
+        },
         update: function(delta) {
             var body = this.player.getComponent('RectPhysicsBody'),
                 sprite = this.player.getComponent('Sprite');
@@ -456,7 +464,8 @@ PlayerMovement.prototype._states = {
             sprite.pivot.x = 0;
             body.mass = 1;
             body.solid = true;
-            body.velocity.y = 0;
+            // Do this to avoid going to a "stand" state
+            body.velocity.y = 1e-7;
             body.x = 400;
             body.y = 450;
         }
